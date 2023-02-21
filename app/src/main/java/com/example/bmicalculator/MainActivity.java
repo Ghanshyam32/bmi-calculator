1728e876103f5532e496b0e110eb176cb3d06710
Ghanshyam Mishra <ghanshyammishra3205615@gmail.com>
2023-02-21 18:15:06 +0530

    logic part

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,12 +1,32 @@
 package com.example.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mCurrentHeight, mCurrentAge, mCurrentWeight;
+    ImageView mIncrementAge, mIncrementWeight, mDecrementWeight, mDecrementAge;
+    SeekBar mSeekbar;
+    RelativeLayout mMale, mFemale;
+
+    int intWeight = 55;
+    int intAge = 22;
+    int currentProgress;
+    String mProgress = "170";
+    String typeOfUser = "0";
+    String weight = "55";
+    String age = "22";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +35,120 @@ public class MainActivity extends AppCompatActivity {
 
         Button calculateBtn = findViewById(R.id.calculate);
 
+        mCurrentAge = findViewById(R.id.currentAge);
+        mCurrentHeight = findViewById(R.id.currentHeight);
+        mCurrentWeight = findViewById(R.id.currentWeight);
+        mIncrementAge = findViewById(R.id.incrementAge);
+        mIncrementWeight = findViewById(R.id.incrementWeight);
+        mDecrementWeight = findViewById(R.id.decrementWeight);
+        mDecrementAge = findViewById(R.id.decrementAge);
+        mSeekbar = findViewById(R.id.seekbarforheight);
+        mMale = findViewById(R.id.male);
+        mFemale = findViewById(R.id.female);
+
+
+        mMale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMale.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selectbg));
+                mFemale.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.notselect));
+                typeOfUser = "Male";
+            }
+        });
+
+        mFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFemale.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.selectbg));
+                mMale.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.notselect));
+                typeOfUser = "Female";
+            }
+        });
+
+
+        mSeekbar.setMax(300);
+        mSeekbar.setProgress(170);
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                currentProgress = i;
+                mProgress = String.valueOf(currentProgress);
+                mCurrentHeight.setText(mProgress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        mIncrementAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intAge = intAge + 1;
+                age = String.valueOf(intAge);
+                mCurrentAge.setText(age);
+            }
+        });
+
+
+        mIncrementWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intWeight = intWeight + 1;
+                weight = String.valueOf(intWeight);
+                mCurrentWeight.setText(weight);
+            }
+        });
+
+        mDecrementWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intWeight = intWeight - 1;
+                weight = String.valueOf(intWeight);
+                mCurrentWeight.setText(weight);
+            }
+        });
+
+        mDecrementAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intAge = intAge - 1;
+                age = String.valueOf(intAge);
+                mCurrentAge.setText(age);
+            }
+        });
+
+
         calculateBtn.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-            startActivity(intent);
-            finish();
+            if (typeOfUser.equals("0")) {
+                Toast.makeText(getApplicationContext(), "Select Your Gender First", Toast.LENGTH_SHORT).show();
+            } else if (mProgress.equals("0")) {
+                Toast.makeText(getApplicationContext(), "Select Your Height First", Toast.LENGTH_SHORT).show();
+            } else if (intAge == 0 || intAge < 0) {
+                Toast.makeText(getApplicationContext(), "Age is Incorrect", Toast.LENGTH_SHORT).show();
+            } else if (intWeight == 0 || intWeight < 0) {
+                Toast.makeText(getApplicationContext(), "Weight is Incorrect", Toast.LENGTH_SHORT).show();
+            } else {
+
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+
+                intent.putExtra("gender", typeOfUser);
+                intent.putExtra("height", mProgress);
+                intent.putExtra("weight", weight);
+                intent.putExtra("age", age);
+
+
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/example/bmicalculator/ResultActivity.java
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.java
@@ -3,10 +3,27 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+
+    TextView bmiDisplay, bmiCategory, mGender;
+    Intent intent;
+    ImageView imageView;
+    String bmi;
+    float fBmi;
+
+    String height;
+    String weight;
+    float intHeight, intWeight;
+    RelativeLayout background;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +31,46 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result_activtiy);
 
         Button recalculateBtn = findViewById(R.id.recalculate);
+
+        intent = getIntent();
+        bmiDisplay = findViewById(R.id.displayBmi);
+        bmiCategory = findViewById(R.id.category);
+        mGender = findViewById(R.id.resultGender);
+        background = findViewById(R.id.resultLayout);
+        imageView = findViewById(R.id.resultImage);
+
+        height = intent.getStringExtra("height");
+        weight = intent.getStringExtra("weight");
+        intHeight = Float.parseFloat(height);
+        intWeight = Float.parseFloat(weight);
+
+        intHeight = intHeight / 100;
+        fBmi = intWeight / (intHeight * intHeight);
+
+        bmi = Float.toString(fBmi);
+
+        if (fBmi < 16) {
+            bmiCategory.setText("Severe Thinnes");
+            imageView.setImageResource(R.drawable.crosspng);
+        } else if (fBmi < 16.9 && fBmi > 16) {
+            bmiCategory.setText("Moderate Thinnes");
+            imageView.setImageResource(R.drawable.warning);
+        } else if (fBmi < 18.4 && fBmi > 17) {
+            bmiCategory.setText("Mild Thinnes");
+            imageView.setImageResource(R.drawable.warning);
+        } else if (fBmi < 25 && fBmi > 18.5) {
+            bmiCategory.setText("Normal");
+            imageView.setImageResource(R.drawable.tick);
+        } else if (fBmi < 29.4 && fBmi > 24.9) {
+            bmiCategory.setText("Overweight");
+            imageView.setImageResource(R.drawable.warning);
+        } else {
+            bmiCategory.setText("Obese");
+            imageView.setImageResource(R.drawable.crosspng);
+        }
+
+        mGender.setText(intent.getStringExtra("gender"));
+        bmiDisplay.setText(bmi);
 
         recalculateBtn.setOnClickListener(view -> {
             Intent i = new Intent(getApplicationContext(), MainActivity.class);
